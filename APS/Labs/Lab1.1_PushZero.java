aeac9a313b0a00a9cf44da207e71edca7fe44b4f
vase42 <124687007+vase42@users.noreply.github.com>
2023-02-07 13:57:28 +0100

    Update Lab1.1_PushZero.java

--- APS/Labs/Lab1.1_PushZero.java
+++ APS/Labs/Lab1.1_PushZero.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class PushZero
+{
+    public static void pushZerosToEnd(int arr[], int n){
+        int k=0;
+        int [] aot = new int[n];
+        System.out.println("Transformiranata niza e: ");
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0) {
+                System.out.print(arr[i] + " ");
+                k++;
+            }
+        }
+        for(int i=0;i<n-k;i++){
+            System.out.print(0 + " ");
+        }
+    }
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int arr[] = new int[100];
+        int n = scanner.nextInt();
+        for(int i=0,k=0;i<n;i++) {
+            arr[i] = scanner.nextInt();
+        }
+        pushZerosToEnd(arr, n);
+    }
+}
